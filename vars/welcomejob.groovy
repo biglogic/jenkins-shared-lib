0fd8020d61d78e75c61004bca3cfecd4f7ef674f
atul singh <atulsinghtanakpur@gmail.com>
2021-06-23 18:30:35 +0530

    .

--- vars/welcomejob.groovy
+++ vars/welcomejob.groovy
@@ -1,28 +1,7 @@
-def call(String name = 'User') {
-		echo "$name"
+def call(String jenkins_pipeline_name = 'User' , jenkins_lib_name = 'User' ) {
                 pipeline {
                   agent any
                    stages {
-                      stage('Setup parameters') {
-                            steps {
-                              script { 
-                                 properties([
-                                     parameters([
-                                        string(
-                                          defaultValue: 'Firstjob_with_shared_lib', 
-                                          name: 'jenkins_pipeline_name', 
-                                          trim: true
-                                        ),
-                                        string(
-                                          defaultValue: 'first_jenkins_shared',
-                                          name: 'jenkins_lib_name',
-                                          trim: true
-                                        ) 
-                                   ])
-                               ])
-                           } 
-                       }
-                      }
                       stage('Hello') {
                           steps {
                                sh " bash ../$jenkins_pipeline_name@libs/$jenkins_lib_name/jenkins-shared.sh"
