e3fe7e98a885282763a2e20f338b3a2f210ca979
atul singh <atulsinghtanakpur@gmail.com>
2021-06-23 17:50:29 +0530

    .

--- vars/welcomejob.groovy
+++ vars/welcomejob.groovy
@@ -3,7 +3,21 @@ def call(String name = 'User') {
                 pipeline {
                   agent any
                    stages {
-                     stage('Hello') {
+                      stage('Setup parameters') {
+                            steps {
+                              script { 
+                                 properties([
+                                     parameters([
+                                        string(
+                                          defaultValue: 'Firstjob-with_shared_lib', 
+                                          name: 'jenkins_pipeline_name', 
+                                          trim: true
+                                        )
+                                   ])
+                               ])
+                           } 
+                       }
+                      stage('Hello') {
                           steps {
                                sh " bash ../Firstjob_with_shared_lib@libs/first_jenkins_shared/jenkins-shared.sh"
            }
@@ -11,4 +25,4 @@ def call(String name = 'User') {
     }
 }
 
-}
+
